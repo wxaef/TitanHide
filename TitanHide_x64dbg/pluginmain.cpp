26df0288494b10dbac383ef6107b9928f11a2b93
wxaef <134511785+wxaef@users.noreply.github.com>
2026-09-25 10:31:58 +0800

    Add visible TitanHide plugin menu

--- TitanHide_x64dbg/pluginmain.cpp
+++ TitanHide_x64dbg/pluginmain.cpp
@@ -8,6 +8,15 @@ int hMenuDisasm;
 int hMenuDump;
 int hMenuStack;
 
+enum TITANHIDE_MENU_ID
+{
+    MENU_HIDE = 1,
+    MENU_UNHIDE,
+    MENU_MODE_USER,
+    MENU_MODE_AUTO,
+    MENU_SHOW_MODE
+};
+
 PLUG_EXPORT bool pluginit(PLUG_INITSTRUCT* initStruct)
 {
     initStruct->pluginVersion = PLUGIN_VERSION;
@@ -26,4 +35,43 @@ PLUG_EXPORT bool plugstop()
 
 PLUG_EXPORT void plugsetup(PLUG_SETUPSTRUCT* setupStruct)
 {
+    hwndDlg = setupStruct->hwndDlg;
+    hMenu = setupStruct->hMenu;
+    hMenuDisasm = setupStruct->hMenuDisasm;
+    hMenuDump = setupStruct->hMenuDump;
+    hMenuStack = setupStruct->hMenuStack;
+
+    _plugin_menuaddentry(hMenu, MENU_HIDE, "Hide current debuggee");
+    _plugin_menuaddentry(hMenu, MENU_UNHIDE, "Unhide");
+    _plugin_menuaddseparator(hMenu);
+    _plugin_menuaddentry(hMenu, MENU_MODE_USER, "Use user mode (driverless)");
+    _plugin_menuaddentry(hMenu, MENU_MODE_AUTO, "Use auto mode");
+    _plugin_menuaddentry(hMenu, MENU_SHOW_MODE, "Show current mode");
+}
+
+PLUG_EXPORT void CBMENUENTRY(CBTYPE cbType, PLUG_CB_MENUENTRY* info)
+{
+    if(!info)
+        return;
+
+    switch(info->hEntry)
+    {
+    case MENU_HIDE:
+        DbgCmdExecDirect("TitanHide");
+        break;
+    case MENU_UNHIDE:
+        DbgCmdExecDirect("TitanUnhide");
+        break;
+    case MENU_MODE_USER:
+        DbgCmdExecDirect("TitanHideMode user");
+        break;
+    case MENU_MODE_AUTO:
+        DbgCmdExecDirect("TitanHideMode auto");
+        break;
+    case MENU_SHOW_MODE:
+        DbgCmdExecDirect("TitanHideMode");
+        break;
+    default:
+        break;
+    }
 }
